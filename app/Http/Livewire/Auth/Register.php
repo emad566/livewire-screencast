254e8e0c102c864dd9929d338e9d5e07e6838812
emad566 <emade09@gmail.com>
2023-08-14 20:11:03 +0300

    02-02 Seting up the Form

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Register extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+    public $passwordConfirmation;
+
+    public function register()
+    {
+        User::create([
+            'email'=> $this->email,
+            'name'=> $this->name,
+            'password'=> $this->password,
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.auth.register');
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,4 +1,23 @@
 <div>
 
-Register Form
+    <form action="#" wire:submit.prevent="register" >
+        <div>
+            <label for="name">name</label>
+            <input wire:model="name" type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for="email">email</label>
+            <input wire:model="email" type="email" name="email" id="email">
+        </div>
+        <div>
+            <label for="password">password</label>
+            <input wire:model="password" type="password" name="password" id="password">
+        </div>
+        <div>
+            <label for="passwordConfirmation">passwordConfirmation</label>
+            <input wire:model="passwordConfirmation" type="password" name="passwordConfirmation" id="passwordConfirmation">
+        </div>
+
+        <button type="submit">Submit</button>
+    </form>
 </div>
