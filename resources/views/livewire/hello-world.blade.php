9ca56cb4a92e842e8118e0f74b2ed68b27d141d3
emad566 <emade09@gmail.com>
2023-08-14 12:24:14 +0300

    Intro

---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,7 @@
+<div>
+    Live wire Hello {{ $name }}: {{ $count }}
+
+    <button wire:click="inc">+</button>
+    <button wire:click="dec">-</button>
+    <button wire:click="zero">Zero</button>
+</div>
