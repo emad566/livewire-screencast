c94f5ac1558f52f21d90417a0af568423faa96bc
Emadeldeen Soliman <emade09@gmail.com>
2023-08-18 21:56:17 +0300

    05-02 Extracting Reusable Blade Components

--- app/Http/Livewire/Dashboard/Profile.php
+++ app/Http/Livewire/Dashboard/Profile.php
@@ -9,12 +9,13 @@ class Profile extends Component
     protected $listeners = ['notify-saved'=>'$refresh'];
     public $name;
     public $about;
-    public $saved;
+    public $birthday = null;
 
     function mount()  {
         $user = auth()->user();
         $this->name = $user->name; 
         $this->about = $user->about; 
+        $this->birthday = optional(auth()->user()->birthday)->format('m/d/Y');
     }
 
     public function save()
@@ -22,12 +23,10 @@ class Profile extends Component
         $data = $this->validate([
             'name' => 'required|max:24',
             'about' => 'required|min:24|max:100',
+            'birthday' => 'sometimes',
         ]);
 
-        auth()->user()->update([
-            'name'=>$this->name,
-            'about'=>$this->about,
-        ]);
+        auth()->user()->update($data);
         $this->emitSelf('notify-saved');
     }
 
