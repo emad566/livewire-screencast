ca1a63ee1acd003b5e8c191114a3eb285062edd4
Emadeldeen Soliman <emade09@gmail.com>
2023-08-18 22:50:03 +0300

    05-06 Using A Rich-Text Editor Trix

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,7 +8,13 @@
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
     @vite('resources/css/app.css')
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
+    <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
+
+    <script defer src="{{ asset('js/pikaday.js') }}"></script>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/pikaday.css') }}">
     <script defer src="{{ asset('js/alpine.min.js') }}"></script>
+
 
     <livewire:styles />
 </head>
